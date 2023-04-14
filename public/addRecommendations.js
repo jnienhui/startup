59aeb32f5b2d23c87efba4e04544b2f2b08b328d
jnienhui <jnienhuis132@gmail.com>
2023-04-14 01:53:32 -0600

    Added Websocket functionality

--- public/addRecommendations.js
+++ public/addRecommendations.js
@@ -1,6 +1,9 @@
+RecentRecommendation = 'RecentRecommendation';
 document.addEventListener('DOMContentLoaded', function() {
     const myElement = document.querySelector('.Name');
     myElement.textContent = localStorage.getItem('name');
+    configureWebSocket();
+    boundConfigureWebSocket(); // ensure socket is defined
   });
 
 
@@ -31,6 +34,7 @@ form.addEventListener('submit', function(event) {
   addRecommendation(recommendation);
   
   document.querySelector('#addRecSuccess').textContent = "Recommendation added sucessfully!";
+  broadcastEvent(name,RecentRecommendation ,title);
   form.reset();
 });
 
@@ -49,11 +53,63 @@ async function addRecommendation(recommendation){
   }
 }
 
+function configureWebSocket() {
+  const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+  this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+  this.socket.onopen = (event) => {
+    console.log('Websocket opened');
+  };
+  this.socket.onclose = (event) => {
+    console.log('Websocket closed');
+  };
+  this.socket.onmessage = async (event) => {
+    console.log('Recommendation broadcasted');
+    const msg = JSON.parse(await event.data.text());
+    if (msg.type === RecentRecommendation) {
+      this.displayMsg('user', msg.from, `recently recommended ${msg.value}`);
+    } 
+  };
+}
+
+const boundConfigureWebSocket = configureWebSocket.bind(this);
+
+
+const MAX_MESSAGES = 10;
+function displayMsg(cls, from, msg) {
+  const chatText = document.querySelector('#recent-recommendations');
+  const newRec =
+    `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>`;
+    chatText.insertAdjacentHTML('afterbegin', newRec);
+  // remove the oldest message if the maximum limit is reached
+  if (chatText.children.length > MAX_MESSAGES) {
+    chatText.removeChild(chatText.lastElementChild);
+  }
+}
+
+function broadcastEvent(from, type, value) {
+  const event = {
+    from: from,
+    type: type,
+    value: value,
+  };
+  
+  if (this.socket.readyState === WebSocket.OPEN) {
+    this.socket.send(JSON.stringify(event));
+  } else {
+    console.log('WebSocket connection is not fully open yet');
+    this.socket.addEventListener('open', () => {
+      this.socket.send(JSON.stringify(event));
+    });
+  }
+}
+
 const signoutBtn = document.querySelector('#signout-btn');
 
 // check if the user is signed in
 if (localStorage.getItem('isLoggedin')) {
+  console.log('configuredWebsocket');
   // show the signout button
+  console.log('error configuring websocket did not occur');
   signoutBtn.style.display = 'block';
 } else {
   // hide the signout button
